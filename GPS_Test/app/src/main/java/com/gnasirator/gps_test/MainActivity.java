08b6e0d4027a2e133733e1539f30ec3463a0b62a
Valle <gnasirator@gmail.com>
2017-01-06 22:15:12 +1300

    GPS_Test voll funktionsfaehig wie es scheint: Posi, Speed, Accel UND Orientierung

--- GPS_Test/app/src/main/java/com/gnasirator/gps_test/MainActivity.java
+++ GPS_Test/app/src/main/java/com/gnasirator/gps_test/MainActivity.java
@@ -1,26 +1,42 @@
 package com.gnasirator.gps_test;
 
 import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.location.LocationProvider;
 import android.os.Bundle;
+import android.os.PowerManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Display;
+import android.view.Surface;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 import static java.lang.Boolean.TRUE;
 
-public class MainActivity extends AppCompatActivity implements LocationListener {
+public class MainActivity extends AppCompatActivity implements LocationListener, SensorEventListener {
 
     private LocationManager locationManager;
-    TextView text_out;
+    private PowerManager mPowerManager;
+    private PowerManager.WakeLock mWakeLock;
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer, mOrientation;
+    private float mSensorX, mSensorY, mSensorZ;
+    private WindowManager mWindowManager;
+    private Display mDisplay;
+    TextView txt_posi, txt_speed, txt_accel, txt_orient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +45,27 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        text_out = (TextView) findViewById(R.id.txt_output);
+        txt_posi = (TextView) findViewById(R.id.txt_posiOut);
+        txt_speed = (TextView) findViewById(R.id.txt_speedOut);
+        txt_accel = (TextView) findViewById(R.id.txt_accelOut);
+        txt_orient = (TextView) findViewById(R.id.txt_orientOut);
+
+        mWindowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+        mDisplay = mWindowManager.getDefaultDisplay();
+
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_NOSENSOR);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        // Get an instance of the SensorManager
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        // Get an instance of the PowerManager
+        mPowerManager = (PowerManager) getSystemService(POWER_SERVICE);
+
+        // Create a bright wake lock
+        mWakeLock = mPowerManager.newWakeLock(PowerManager.SCREEN_BRIGHT_WAKE_LOCK, getClass().getName());
+
 
         /*
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -43,28 +79,35 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
 
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
-        try{locationManager.requestLocationUpdates( LocationManager.GPS_PROVIDER,
-                3000,   // 3 sec
-                10, this);}
-        catch (SecurityException e) {
-            text_out.setText("Error: Security!");
+        try {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                    1000,   // 3 sec
+                    1,     // 10m min distance update
+                    this);
+        } catch (SecurityException e) {
+            txt_posi.setText("Error: Security!");
         }
 
-        if (locationManager != null) {
-            Location location;
-            try {
-                location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                String str = "Latitude: "+location.getLatitude()+" Longitude: "+location.getLongitude();
-                text_out.append("\n" + str);
-                String speed = location.hasSpeed() == TRUE ? "1" : "0";
-                text_out.append("\nhas:\t" + speed);
-                speed = Float.toString(location.getSpeed());
-                text_out.append("\nv:\t" + speed);
-                text_out.append("\np:\t" + location.getProvider());
-            } catch (SecurityException e) {
-                text_out.setText("Error: Security2!");
-            }
-        }
+//        if (locationManager != null) {
+//            Location location;
+//            try {
+//                location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+//                String str = "Latitude: "+location.getLatitude()+" Longitude: "+location.getLongitude();
+//                text_out.append("\n" + str);
+//                String speed = location.hasSpeed() == TRUE ? "1" : "0";
+//                text_out.append("\nhas:\t" + speed);
+//                speed = Float.toString(location.getSpeed());
+//                text_out.append("\nv:\t" + speed);
+//                text_out.append("\np:\t" + location.getProvider());
+//            } catch (SecurityException e) {
+//                text_out.setText("Error: Security2!");
+//            }
+//        }
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+
+        mOrientation = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+        mSensorManager.registerListener(this, mOrientation, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
@@ -98,13 +141,12 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
      */
     @Override
     public void onLocationChanged(Location location) {
-        String str = "Latitude: "+location.getLatitude()+" Longitude: "+location.getLongitude();
-        text_out.append("\n" + str);
-        String speed = location.hasSpeed() == TRUE ? "1" : "0";
-        text_out.append("\nhas:\t" + speed);
-        speed = Float.toString(location.getSpeed());
-        text_out.append("\nv:\t" + speed);
-        text_out.append("\np:\t" + location.getProvider());
+        txt_posi.setText("Lat:\t\t" + location.convert(Math.abs(location.getLatitude()), location.FORMAT_SECONDS) + "\t˚ " + (location.getLatitude() > 0 ? "N" : "S") +
+                "\nLong:\t" + location.convert(Math.abs(location.getLongitude()), location.FORMAT_SECONDS) + "\t˚ " + (location.getLongitude() > 0 ? "E" : "W") +
+                "\nAlt:\t\t" + String.format("%1$,.1f",location.getAltitude()) + "\tm");
+        txt_speed.setText(String.format("%1$,.1f", location.getSpeed()*3.6) + "\tkm/h");
+        txt_speed.append("\n" + String.format("%1$,.1f", location.getBearing()) + "˚");
+
     }
 
 
@@ -133,7 +175,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
      */
     @Override
     public void onStatusChanged(String provider, int status, Bundle extras) {
-        text_out.append("stat_changed");
+//        text_out.append("stat_changed");
     }
 
     /**
@@ -144,7 +186,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
      */
     @Override
     public void onProviderEnabled(String provider) {
-        text_out.setText("provider enabled");
+//        text_out.setText("provider enabled");
     }
 
     /**
@@ -157,6 +199,105 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
      */
     @Override
     public void onProviderDisabled(String provider) {
-        text_out.setText("provider disabled");
+
+//        text_out.setText("provider disabled");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        /*
+         * when the activity is resumed, we acquire a wake-lock so that the
+         * screen stays on, since the user will likely not be fiddling with the
+         * screen or buttons.
+         */
+        mWakeLock.acquire();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        /*
+         * When the activity is paused, we make sure to stop the simulation,
+         * release our sensor resources and wake locks
+         */
+
+        // and release our wake-lock
+        mWakeLock.release();
+    }
+
+    /**
+     * Called when there is a new sensor event.  Note that "on changed"
+     * is somewhat of a misnomer, as this will also be called if we have a
+     * new reading from a sensor with the exact same sensor values (but a
+     * newer timestamp).
+     * <p>
+     * <p>See {@link SensorManager SensorManager}
+     * for details on possible sensor types.
+     * <p>See also {@link SensorEvent SensorEvent}.
+     * <p>
+     * <p><b>NOTE:</b> The application doesn't own the
+     * {@link SensorEvent event}
+     * object passed as a parameter and therefore cannot hold on to it.
+     * The object may be part of an internal pool and may be reused by
+     * the framework.
+     *
+     * @param event the {@link SensorEvent SensorEvent}.
+     */
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+
+            /*
+             * record the accelerometer data, the event's timestamp as well as
+             * the current time. The latter is needed so we can calculate the
+             * "present" time during rendering. In this application, we need to
+             * take into account how the screen is rotated with respect to the
+             * sensors (which always return data in a coordinate space aligned
+             * to with the screen in its native orientation).
+             */
+
+            switch (mDisplay.getRotation()) {
+                case Surface.ROTATION_0:
+                    mSensorX = event.values[0];
+                    mSensorY = event.values[1];
+                    break;
+                case Surface.ROTATION_90:
+                    mSensorX = -event.values[1];
+                    mSensorY = event.values[0];
+                    break;
+                case Surface.ROTATION_180:
+                    mSensorX = -event.values[0];
+                    mSensorY = -event.values[1];
+                    break;
+                case Surface.ROTATION_270:
+                    mSensorX = event.values[1];
+                    mSensorY = -event.values[0];
+                    break;
+            }
+            mSensorZ = event.values[2];
+            txt_accel.setText("X:\t" + String.format("%1$,.2f", mSensorX/9.81) + "\tg\nY:\t" + String.format("%1$,.2f",mSensorY/9.81) + "\tg\nZ:\t" + String.format("%1$,.2f",mSensorZ/9.81) + "\tg");
+        } else if (event.sensor.getType() == Sensor.TYPE_ROTATION_VECTOR) {
+            txt_orient.setText(String.format("%1$,.2f", Math.asin(event.values[0])*360/Math.PI) + "˚"
+                    + "\n" + String.format("%1$,.2f", Math.asin(event.values[1])*360/Math.PI) + "˚"
+                    + "\n" + String.format("%1$,.2f", Math.asin(event.values[2])*360/Math.PI) + "˚"
+            ); //String.format("%1$,.2f", myDouble);
+        }
+    }
+
+    /**
+     * Called when the accuracy of the registered sensor has changed.  Unlike
+     * onSensorChanged(), this is only called when this accuracy value changes.
+     * <p>
+     * <p>See the SENSOR_STATUS_* constants in
+     * {@link SensorManager SensorManager} for details.
+     *
+     * @param sensor
+     * @param accuracy The new accuracy of this sensor, one of
+     *                 {@code SensorManager.SENSOR_STATUS_*}
+     */
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
